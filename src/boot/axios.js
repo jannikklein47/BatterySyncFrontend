9e953042f28da0ecf7ec02ef22588c2d8141da2b
jannikklein47 <jannikklein0302@outlook.de>
2025-12-01 11:39:27 +0100

    changed: domain

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: 'https://batterysync.chickenkiller.com:3000' })
+const api = axios.create({ baseURL: 'https://batterysync.de:3000' })
 
 api.interceptors.request.use(function (config) {
   const auth = localStorage.getItem('accessToken')
