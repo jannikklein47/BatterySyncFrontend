e2981c6e6647473398b25065433c615e2309981d
jannikklein47 <jannikklein0302@outlook.de>
2026-05-26 15:24:04 +0200

    removed: dashboard button when not logged in

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -35,6 +35,7 @@
             class="nav-btn"
             :class="{ 'active-page': $route.name === 'dashboard' }"
             :to="`/${$route.params.locale}/dashboard`"
+            v-if="isLoggedIn"
           />
           <q-btn
             flat
